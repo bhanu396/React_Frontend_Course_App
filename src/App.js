9f0a7d589493085cf67dba29368fd1711b6a81ba
bhanu396 <bhanuprakashgujjari@gmail.com>
2022-10-18 16:36:09 +0530

    React component part 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent.js';
+import { DISHES } from './shared/dishes';
+
 function App() {
-  return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-      </div>
-    );
+    const dishes = DISHES;
+      return (
+        <div className="App">
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            </div>
+          </Navbar>
+          <Menu dishes={dishes} />
+        </div>
+      );
 }
+
+// function App() {
+//   this.state = {
+//     dishes: DISHES
+//   };
+//   return (
+//       <div className="App">
+//         <Navbar dark color="primary">
+//           <div className="container">
+//             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+//           </div>
+//         </Navbar>
+//         <Menu />
+//       </div>
+//     );
+// }
 
 export default App;
