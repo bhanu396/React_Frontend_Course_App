77b3bb68d25c2f541e8ce2aae3d3e19dd2e46cea
bhanu396 <bhanuprakashgujjari@gmail.com>
2023-02-15 16:45:14 +0530

    Assignment 4

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,31 +1,41 @@
 import React from 'react';
-import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Fade, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { baseUrl } from '../shared/baseUrl';
+import {Stagger} from 'react-animation-components'
 
 function RenderLeader({leader}){
     return (
-        <div key={leader.id} className="col-12 mt-5">
-         <Media tag="li">
-          <Media left middle>
-            <Media object src={leader.image} alt={leader.name} />
-          </Media>
-          <Media body className="ml-5">
-           <Media heading>{leader.name}</Media>
-           <p>{leader.designation}</p>
-           <p>{leader.description}</p>
-          </Media>
-         </Media>
-        </div>
+            <Fade in>
+            <div key={leader.id} className="col-12 mt-5">
+                <Media tag="li">
+                <Media left middle>
+                    <Media object src={baseUrl + leader.image} alt={leader.name} />
+                </Media>
+                <Media body className="ml-5">
+                <Media heading>{leader.name}</Media>
+                <p>{leader.designation}</p>
+                <p>{leader.description}</p>
+                </Media>
+                </Media>
+            </div>
+            </Fade>
+        
        );
 } 
 function About(props) {
 
-    const leaders = props.leaders.map((leader) => {
-        return (
-            <div>
+    const leaders = props.leaders.leaders.map((leader) => {
+        
+            return (
+                
+                <div>
                 <RenderLeader leader={leader}/>
-            </div>
-        );
+                </div>
+                
+            );
+        
+        
     });
 
     return(
@@ -84,7 +94,10 @@ function About(props) {
                 </div>
                 <div className="col-12">
                     <Media list>
-                        {leaders}
+                        <Stagger in>
+                            {leaders}
+                        </Stagger>
+                        
                     </Media>
                 </div>
             </div>
